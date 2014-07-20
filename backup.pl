c8985c08f1f98266b9e0f054b172e88bbdcd7432
Harry <nux.harry@gmail.com>
2014-07-20 18:34:50 +1200

    Added disk wise folder with date n time appended at dir level

--- backup.pl
+++ backup.pl
@@ -12,10 +12,6 @@
 #         BUGS: ---
 #        NOTES: ---
 #       AUTHOR: Harry (nux.harry@gmail.com)
-# ORGANIZATION: 
-#      VERSION: 1.0
-#      CREATED: 07/20/2014 10:54:04 AM
-#     REVISION: ---
 #===============================================================================
 
 use strict;
@@ -25,14 +21,15 @@ use utf8;
 use 5.010;
 use File::Copy;
 use Parallel::ForkManager;
-
+use POSIX 'strftime';
 
 my $pm = Parallel::ForkManager->new(4);
 
 #### user enter guest vm name to take backup.
 ### gather disk info as per vm name
-
+my $dest_dir = '/tmp';
 my @vm_disk;
+my $date = strftime '%Y-%m-%d', localtime;
 for my $vm (@ARGV){
 	push @vm_disk, `virsh domblklist $vm | grep vda | awk -F ' ' {'print \$2'}`;
 }
@@ -43,9 +40,14 @@ for my $disk (@vm_disk){
 }
 foreach my $data (@vm_disk) {
      my $pid = $pm->start and next;
+		$data =~ m/.*\/(.*?)$/;
 		chomp $data;
- 	  copy $data, "${data}_tejas" or die "Error $!";
+	  say "Backup is started for $data..................";
+	  say "...............";
+ 	  mkdir "$dest_dir/$1_$date"; 
+	   copy $data, "$dest_dir/$1_$date" or die "Error $!";
            $pm->finish; 
+	say ".........Finished for $data";
            }
 $pm->wait_all_children;
 
